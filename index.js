32606042a7fa32ea74efcf5d4f91af38ebdd4223
ammarahm69 <ammar69aa59@gmail.com>
2025-04-22 20:05:42 +0500

    update

--- index.js
+++ index.js
@@ -18,7 +18,13 @@ app.use(express.json());
 app.use(cors());
 
 // DB Connect
-mongoose.connect(process.env.MONGODB_URI, console.info("🚀 MongoDB Connected"));
+mongoose
+  .connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.info("🚀 MongoDB Connected"))
+  .catch((err) => console.error("❌ MongoDB connection error:", err));
 
 // Cloudinary Connect
 connectCloudinary();
